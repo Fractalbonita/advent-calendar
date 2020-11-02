a71c59edebc74d3cc6b8fdc54494822dbfdddf8f
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-02 17:27:20 +0100

    Add app component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header msg="Advent calendar" />
+    <CalendarWindow />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Header from './components/Header.vue';
+import CalendarWindow from './components/CalendarWindow.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Header,
+    CalendarWindow
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
