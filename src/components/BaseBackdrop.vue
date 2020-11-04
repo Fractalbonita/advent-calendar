f3f62deb378e03b9a5884d9d6021342b63dcd693
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-05 00:30:05 +0100

    Update backdrop

--- src/components/BaseBackdrop.vue
+++ src/components/BaseBackdrop.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="shown"></div>
+  <div v-if="backdrop" v-on:click="handleClick"></div>
 </template>
 
 <script lang="ts">
@@ -8,7 +8,8 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'BaseBackdrop',
   props: {
-    shown: Boolean
+    backdrop: Boolean,
+    handleClick: Function
   }
 });
 </script>
