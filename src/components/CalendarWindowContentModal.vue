26760c17a7505fd39bad40bc9dbd5577297b4bbb
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-06 14:47:25 +0100

    Outsource calendarwindowcontentmodal

---
+++ src/components/CalendarWindowContentModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <BaseModal v-bind:closeModal="close" v-bind:scrollable="false">
+    <template v-slot:header
+      ><h1>{{ name }}</h1>
+      <p>{{ brewery }}</p></template
+    >
+    <template v-slot:main>
+      <p>Image</p>
+      <p>{{ category }}</p>
+      <p>{{ award }}</p>
+      <p>{{ year }}</p>
+      <p>{{ teaste }}</p>
+      <p>{{ abv }}</p>
+      <p>{{ country }}</p>
+      <p>{{ tastingNote }}</p>
+      <a v-bind:href="website" target="_blank" rel="noopener noreferrer">{{
+        website
+      }}</a>
+    </template>
+  </BaseModal>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BaseModal from './BaseModal.vue';
+
+export default Vue.extend({
+  name: 'CalendarWindowContentModal',
+  components: {
+    BaseModal
+  },
+  props: {
+    close: Function,
+    award: String,
+    year: String,
+    name: String,
+    brewery: String,
+    tastingNote: String,
+    category: String,
+    country: String,
+    teaste: String,
+    abv: String,
+    website: String
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
