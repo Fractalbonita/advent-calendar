f3c62947e0293a1c8c284fb61074bf16a27bd49e
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-06 18:29:20 +0100

    Fix json data

--- json-server/jsonServer.js
+++ json-server/jsonServer.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const db = require('./db.json');
 // Improve random pick by using the Fisher-Yates Algorithm, since this method has bias
-const beers = db.beers.sort(() => (Math.random() > 0.5 ? 1 : -1));
 
 module.exports = () => {
-  return { beers };
+  return { beers: db.beers.sort(() => (Math.random() > 0.5 ? 1 : -1)) };
 };
