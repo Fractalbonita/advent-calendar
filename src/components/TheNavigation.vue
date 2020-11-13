8314b3eb797097df4bda93c3a9bae7ac2f28a24f
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-13 14:01:51 +0100

    Add navigation

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,25 +1,48 @@
 <template>
   <nav class="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/calendar">Calendar</router-link>
-    <router-link to="/favorites">Favorites</router-link>
-    <router-link to="/beers">Beers</router-link>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/calendar">Calendar</router-link>
+      </li>
+      <li>
+        <router-link to="/favorites">Favorites</router-link>
+      </li>
+      <li>
+        <router-link to="/beers">Beers</router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
 <style lang="scss" scoped>
 .nav {
-  padding: 30px;
+  display: flex;
+  justify-content: space-evenly;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  border-bottom: 1px solid $primary-color;
+  z-index: 1;
 
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+  }
   a {
     font-weight: bold;
-    color: #2c3e50;
-    padding: 0 10px;
+    color: $secondary-color;
+    padding: 10px;
     text-decoration: none;
 
     &.router-link-exact-active {
       text-decoration: underline;
-      color: #42b983;
+      color: $primary-color;
     }
   }
 }
