60aeb2354a758ca0b6e4243957e75de9cc799f60
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-13 13:58:39 +0100

    Create not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <h2>Page not found.</h2>
+    <p>
+      Please go back
+    </p>
+    <router-link v-bind:to="{ name: 'Home' }">Home</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'NotFound'
+});
+</script>
