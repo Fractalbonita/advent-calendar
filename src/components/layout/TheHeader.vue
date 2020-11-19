e0d80349ba45ef5badf4a4bcdfbc13ce1ad0f23e
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-19 14:19:06 +0100

    Introduce layout folder

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">{{ title }}</h1>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'TheHeader',
+  props: {
+    title: String
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+  text-align: center;
+
+  &__title {
+    font-size: $headline-1-size;
+    margin: 0.5rem;
+  }
+}
+</style>
