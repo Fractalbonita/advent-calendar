456426065d4cccc67905cb2894e798387b1d6da7
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-19 14:18:17 +0100

    Rafactor navigation

---
+++ src/components/layout/TheNavigation.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="nav">
+    <ul class="nav__wrapper">
+      <li v-for="link in links" v-bind:key="link.path" class="nav__link">
+        <BaseNavLink v-bind:link="link" />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BaseNavLink from '../ui/BaseNavLink.vue';
+
+export default Vue.extend({
+  name: 'TheNavigation',
+  components: {
+    BaseNavLink
+  },
+  data() {
+    return {
+      links: [
+        {
+          path: '/',
+          name: 'Home'
+        },
+        {
+          path: '/calendar',
+          name: 'Calendar'
+        },
+        {
+          path: '/beers',
+          name: 'Beers'
+        },
+        {
+          path: '/favourites',
+          name: 'Favourites'
+        }
+      ]
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  justify-content: space-evenly;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  border-bottom: 1px solid $primary-color;
+  z-index: 1;
+
+  &__wrapper {
+    list-style: none;
+    padding: 0;
+  }
+
+  &__link {
+    display: inline-block;
+  }
+}
+</style>
---
+++ src/components/ui/BaseNavLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link v-bind:to="link.path">{{ link.name }}</router-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BaseNavLink',
+  props: {
+    link: Object
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+a {
+  font-weight: bold;
+  color: $secondary-color;
+  padding: 10px;
+  text-decoration: none;
+
+  &.router-link-exact-active {
+    text-decoration: underline;
+    color: $primary-color;
+  }
+}
+</style>
