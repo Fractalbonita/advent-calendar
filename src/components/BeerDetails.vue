9fee76be683e7303b5f81b02606d095c0151f520
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-26 15:08:47 +0100

    Add beercommentaddform to beerdetails

--- src/components/BeerDetails.vue
+++ src/components/BeerDetails.vue
@@ -1,34 +1,41 @@
 <template>
   <div>
-    <p>{{ beer.brewery }}</p>
-    <h1>{{ beer.name }}</h1>
-    <img v-bind:src="getImage()" v-bind:alt="beer.name" />
-    <h2>Award</h2>
-    <p>{{ beer.award }}</p>
-    <h2>Year</h2>
-    <p>{{ beer.year }}</p>
-    <p>{{ beer.tastingNote }}</p>
-    <h2>Category</h2>
-    <p>{{ beer.category }}</p>
-    <h2>Style</h2>
-    <p>{{ beer.style }}</p>
-    <h2>Alcohol by volume</h2>
-    <p>{{ beer.abv }}</p>
-    <h2>Country</h2>
-    <p>{{ beer.country }}</p>
-    <h2>Website</h2>
-    <a v-bind:href="beer.website" target="_blank" rel="nopener noreferrer">{{
-      beer.website
-    }}</a>
+    <div>
+      <p>{{ beer.brewery }}</p>
+      <h1>{{ beer.name }}</h1>
+      <img v-bind:src="getImage()" v-bind:alt="beer.name" />
+      <h2>Award</h2>
+      <p>{{ beer.award }}</p>
+      <h2>Year</h2>
+      <p>{{ beer.year }}</p>
+      <p>{{ beer.tastingNote }}</p>
+      <h2>Category</h2>
+      <p>{{ beer.category }}</p>
+      <h2>Style</h2>
+      <p>{{ beer.style }}</p>
+      <h2>Alcohol by volume</h2>
+      <p>{{ beer.abv }}</p>
+      <h2>Country</h2>
+      <p>{{ beer.country }}</p>
+      <h2>Website</h2>
+      <a v-bind:href="beer.website" target="_blank" rel="nopener noreferrer">{{
+        beer.website
+      }}</a>
+    </div>
+    <BeerCommentAddForm v-bind:beer="beer" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Beer from './Beer';
+import BeerCommentAddForm from '../components/BeerCommentAddForm.vue';
 
 export default Vue.extend({
   name: 'BeerDetails',
+  components: {
+    BeerCommentAddForm
+  },
   data() {
     return {
       beers: [] as Beer[]
@@ -59,7 +66,7 @@ export default Vue.extend({
     // type annoation necessary including type "undefined" in csee of no match
     beer(): Beer {
       return (
-        this.beers.find((beer: Beer) => beer.slug === this.slug) || ({} as Beer)
+        this.beers.find((beer: Beer) => this.slug === beer.slug) || ({} as Beer)
       );
     }
   }
