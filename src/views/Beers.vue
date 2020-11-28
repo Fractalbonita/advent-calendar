df835d7e499a2697623b0d22c62548dd6ef6c765
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-28 01:16:42 +0100

    Add basescrolltotop to beers page

--- src/views/Beers.vue
+++ src/views/Beers.vue
@@ -2,6 +2,7 @@
   <div id="beers">
     <TheHeader title="German craft beer award winners" />
     <BeerList />
+    <BaseScrollToTop />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 import Vue from 'vue';
 import TheHeader from '../components/layout/TheHeader.vue';
 import BeerList from '../components/BeerList.vue';
+import BaseScrollToTop from '../components/ui/BaseScrollToTop.vue';
 
 export default Vue.extend({
   name: 'Beers',
   components: {
     TheHeader,
-    BeerList
+    BeerList,
+    BaseScrollToTop
   }
 });
 </script>
