96692a4fb97844f79dd192b628656ffb557effba
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-11-30 00:28:57 +0100

    Add scrollbehaviour

--- src/router/index.ts
+++ src/router/index.ts
@@ -53,7 +53,14 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   // mode: 'history',
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
 
 export default router;
